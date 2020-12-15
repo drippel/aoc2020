67bf4a8661c0aca443cc6f2327fde0f20ca4eaf0
drippel <drippel@liveareacx.com>
2020-12-15 09:46:59 -0500

    2020 15 02 done

--- src/main/scala/com/pfs/advent/Day15.scala
+++ src/main/scala/com/pfs/advent/Day15.scala
@@ -9,16 +9,17 @@ object Day15 {
     // day08 - last CPU
     // day12 - grid enhancements
     Console.out.println("2020 15...")
-    val ls = test1.split(',').toList
+    val ls = input.split(',').toList
     ls.foreach(Console.out.println(_))
     
-    part1( ls.map( _.toInt ) )
+    // part1( ls.map( _.toInt ) )
+    part2( ls.map( _.toInt ) )
     
   }
   
   def part1( is : List[Int] ) = {
     
-    val limit = 30000000
+    val limit = 2020
     var spoken = ListBuffer[Int]()
     spoken ++= is
     
@@ -51,10 +52,50 @@ object Day15 {
     println( last )
   }
   
+  def part2( is : List[Int] ) = {
+
+    val limit = 30000000
+    // val limit = 2020
+    var spokenTurn = mutable.HashMap[Int,Int]()
+    val zs = is.zipWithIndex.reverse.tail
+    zs.foreach( t => { spokenTurn += ( t._1 -> ( t._2 + 1 ) )})
+    println(spokenTurn)
+
+    def innerPart2( turn : Int, lastSpoken : Int ) : Int = {
+
+      if( turn > limit ) {
+        lastSpoken
+      }
+      else {
+        
+        val next = if( !spokenTurn.contains(lastSpoken) ){
+          // next spoken is 0
+          spokenTurn += ( lastSpoken -> (turn - 1) ) 
+          0
+        }
+        else {
+          
+          // calc the age
+          val prev = spokenTurn(lastSpoken)
+          val age = turn - prev - 1
+          spokenTurn(lastSpoken) = turn - 1
+          age 
+        }
+
+        innerPart2( turn + 1, next )
+      }
+    }
+
+    val last = innerPart2( is.size + 1, is.last )
+
+    println(last)
+    println("done")
+  }
+
   def toLines(src : String ) = src.split("\n").toList.map(_.trim).filter( s => !s.isEmpty )
 
   val test1 = """0,3,6"""
-  val test = """1,3,2"""
+  val test = """3,1,2"""
   
   val input = """14,3,1,0,9,5"""
 
