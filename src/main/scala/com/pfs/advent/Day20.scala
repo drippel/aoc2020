90c9bfd0761740c5846acd4f889011d7430fcbdb
drippel <drippel@liveareacx.com>
2020-12-29 14:11:58 -0500

    2020 20 part 2 done

--- src/main/scala/com/pfs/advent/Day20.scala
+++ src/main/scala/com/pfs/advent/Day20.scala
@@ -64,16 +64,393 @@ object Day20 {
     // calc the n x n size
     val side = math.sqrt( ts.size ).toInt
     
+    println( s"puzzle side: ${side}" )
+    
     val loc = ts.map( t => { ( matchCount( t, ts ), t ) } )
     
-    val corners = loc.filter( _._1 == 2 )
+    var corners = loc.filter( _._1 == 2 )
     val sides = loc.filter( _._1 == 3 )
     val middle = loc.filter( _._1 == 4 )
     
     val cids = corners.map( _._2.id ).map( _.toLong )
+    println(cids)
     val prod = cids.foldLeft(1L)( _ * _ )
     println(prod)
     
+    // start with one of the corners and find the sides on either side
+    
+    val anchor = corners.head
+    val size = anchor._2.data.rows
+    
+    var remaining = others(anchor._2,ts)
+    
+    // lets put this corner in 0,0 - but we also need to rotate it so that the two non matching sides are north,top and west,left
+    val solution = Array.fill[Option[Tile]](side,side)(None)
+    
+    val orig = anchor._2.data
+    val possibles = arrangements(orig).toList.map( cloneTile( anchor._2, _ ) )
+    val sideCounts = possibles.map( t => (t, matchCounts( t, remaining.toList ) ) )
+    val found = sideCounts.filter( t => t._2._1 == 0 && t._2._4 == 0)
+    
+    solution(0)(0) = Some(found.head._1)
+    
+    
+    for( i <- 0 until 10 ) {
+
+      // lets move to the east / left
+      val right = solution( 0 )( i ).get.data.col( size - 1 )
+
+      val m = matchSide( right, remaining.toList )
+
+      // orient top is non matching left match right of previous
+      val lps = arrangements( m.get.data ).toList.map( cloneTile( m.get, _ ) )
+      val lpcounts = lps.map( t => (t, matchCounts( t, remaining.toList )) )
+      val lpfound = lpcounts.filter( t => t._2._1 == 0 && right.equals( t._1.data.col( 0 ) ) )
+      
+      solution(0)(i+1) = Some(lpfound.head._1)
+
+      remaining = others( lpfound.head._1, remaining )
+    }
+    
+    // top / right corner 
+    
+    val right = solution( 0 )( 10 ).get.data.col( size - 1 )
+    val m = matchSide( right, remaining.toList )
+    val lps = arrangements( m.get.data ).toList.map( cloneTile( m.get, _ ) )
+    val lpcounts = lps.map( t => (t, matchCounts( t, remaining.toList )) )
+    val lpfound = lpcounts.filter( t => t._2._1 == 0 && t._2._2 == 0 && right.equals( t._1.data.col( 0 ) ) )
+    solution(0)(11) = Some(lpfound.head._1)
+    remaining = others( lpfound.head._1, remaining )
+
+    for( i <- 0 until 10 ) {
+
+      // lets move to the east / left
+      val bottom = solution( i )( 0 ).get.data.row( size - 1 )
+
+      val m = matchSide( bottom, remaining.toList )
+
+      // orient top is non matching left match right of previous
+      val lps = arrangements( m.get.data ).toList.map( cloneTile( m.get, _ ) )
+      val lpcounts = lps.map( t => (t, matchCounts( t, remaining.toList )) )
+      val lpfound = lpcounts.filter( t => t._2._4 == 0 && bottom.equals( t._1.data.row( 0 ) ) )
+
+      solution(i+1)(0) = Some(lpfound.head._1)
+
+      remaining = others( lpfound.head._1, remaining )
+      
+    }
+
+    val bottom = solution( 10 )( 0 ).get.data.row( size - 1 )
+    val bm = matchSide( bottom, remaining.toList )
+    val bmas = arrangements( bm.get.data ).toList.map( cloneTile( bm.get, _ ) )
+    val bmasCounts = bmas.map( t => (t, matchCounts( t, remaining.toList )) )
+    val bmFound = bmasCounts.filter( t => t._2._3 == 0 && t._2._4 == 0 && bottom.equals( t._1.data.row( 0 ) ) )
+    solution(11)(0) = Some(bmFound.head._1)
+    remaining = others( bmFound.head._1, remaining )
+    
+    // bottom row
+    for( i <- 0 until 10 ) {
+
+      // lets move to the east / left
+      val left = solution( 11 )( i ).get.data.col( size - 1 )
+
+      val m = matchSide( left, remaining.toList )
+
+      // orient top is non matching left match right of previous
+      val lps = arrangements( m.get.data ).toList.map( cloneTile( m.get, _ ) )
+      val lpcounts = lps.map( t => (t, matchCounts( t, remaining.toList )) )
+      val lpfound = lpcounts.filter( t => t._2._3 == 0 && left.equals( t._1.data.col( 0 ) ) )
+
+      solution(11)(i+1) = Some(lpfound.head._1)
+
+      remaining = others( lpfound.head._1, remaining )
+
+    }
+
+    // right col 
+    for( i <- 0 until 10 ) {
+
+      // lets move to the east / left
+      val bottom = solution( i )( 11 ).get.data.row( size - 1 )
+
+      val m = matchSide( bottom, remaining.toList )
+
+      // orient top is non matching left match right of previous
+      val lps = arrangements( m.get.data ).toList.map( cloneTile( m.get, _ ) )
+      val lpcounts = lps.map( t => (t, matchCounts( t, remaining.toList )) )
+      val lpfound = lpcounts.filter( t => t._2._2 == 0 && bottom.equals( t._1.data.row( 0 ) ) )
+
+      solution(i+1)(11) = Some(lpfound.head._1)
+
+      remaining = others( lpfound.head._1, remaining )
+
+    }
+    
+    val bt = solution(10)(11).get.data.row(size - 1)
+    val bl = solution(11)(10).get.data.col(size - 1)
+    val bcm = matchSide( bt, remaining.toList )
+    val bcas = arrangements( bcm.get.data ).toList.map( cloneTile( bcm.get, _ ) )
+    val bcasCounts = bcas.map( t => (t, matchCounts( t, remaining.toList )) )
+    val bcFound = bcasCounts.filter( t => t._2._2 == 0 && t._2._3 == 0 && bt.equals( t._1.data.row( 0 ) ) && bl.equals( t._1.data.col(0)) )
+    solution(11)(11) = Some(bcFound.head._1)
+    remaining = others( bcFound.head._1, remaining )
+    
+    
+    // now lets fill in the inside pieces, we are starting at 1,1 and going through to 10,10
+    // matchging all three sides for the last in the row as a checksum
+    // and then matching all four for the last 
+    for( currR <- 1 to 10 ) {
+      for( currC <- 1 to 10 ) {
+
+        // above cell is
+        val above = solution( currR - 1 )( currC )
+        val aboveRow = above.get.data.row( size - 1 )
+        val left = solution( currR )( currC - 1 )
+        val leftCol = left.get.data.col( size - 1 )
+
+        val interiorMatch = matchSide( aboveRow, remaining.toList )
+        val interiorOrientations = arrangements( interiorMatch.get.data ).toList
+                                                                         .map( cloneTile( interiorMatch.get, _ ) )
+        val interiorFound = interiorOrientations.filter( t => {
+          t.data.row( 0 ).equals( aboveRow ) && t.data.col( 0 ).equals( leftCol )
+        } )
+
+        val foundTile = interiorFound.head
+        solution( currR )( currC ) = Some( foundTile )
+        remaining = others( foundTile, remaining )
+      }
+    }
+
+   for( r <- 0 until 12 ) {
+      for( c <- 0 until 12 ) {
+        solution(r)(c) match {
+          case Some(t) => print( s"${t.id} " )
+          case None => print( "UNKN " )
+        }
+      }
+      println(" ")
+    }
+    println(" ")
+    
+    
+    val i = solution(10)(10)
+    val t = solution(9)(10)
+    val r = solution(10)(11)
+    val b = solution(11)(10)
+    val l = solution(10)(9)
+
+    println("test")
+
+    val interiors = Array.ofDim[Tile](side,side)
+    for( ri <- 0 until 12 ) {
+      for( ci <- 0 until 12 ) {
+        val t = solution(ri)(ci)
+        val i = interior(t.get)
+        interiors(ri)(ci) = i
+      }
+    }
+    
+    // now we need to combine into one big 120 x 120 grid
+    val combined = Grid( 96, 96 )
+    
+    for( ro <- 0 until 12 ) {
+      // make a combined row
+      for( ri <- 0 until 8 ) {
+
+        val rc = ListBuffer[Char]()
+        for( gc <- 0 until 12 ) {
+          
+          val i = interiors(ro)(gc)
+          rc ++= i.data.row(ri)
+          
+        }
+        val ridx = (ro * 8) + ri
+        for( i <- 0 until rc.size ) {
+          combined(ridx, i, rc(i) )
+        }
+      }
+    }
+
+    // Grid.print(combined)
+    
+    val combinedOs = arrangements(combined).toList
+    // combinedOs.foreach(Grid.print(_))
+    
+    val mon = makeMonster()
+    
+    /*
+    val msh1 = Grid.parse(mash1)
+    val msh2 = Grid.parse(mash2)
+    
+    println("matches")
+    println( matches( mon, mon ) )
+    println( matches( mon, msh1 ) )
+    println( matches( mon, msh2 ) )
+    println("done")
+     */
+
+    val monMap = mutable.HashMap[Int,List[(Int,Int)]]()
+    var ccnt = 0  
+    for( cmb <- combinedOs ) {
+      var mcnt = 0
+      for( r <- 0 until( cmb.rows - mon.rows + 1 ) ) {
+        for( c <- 0 until( cmb.cols - mon.cols + 1 ) ) {
+          val sub = subGrid( cmb, r, c, mon.rows, mon.cols )
+          val mt = matches( mon, sub )
+          if( mt._1 ) {
+            // Grid.print( sub )
+            val l = monMap.getOrElse(ccnt,List())
+            val adj = mt._2.map( t => { (t._1 + r, t._2 + c) })
+            monMap(ccnt) = (l ++ adj )
+            mcnt = mcnt + 1
+          }
+        }
+      }
+      println( ccnt )
+      println( mcnt )
+      ccnt = ccnt + 1
+    }
+    
+    // 
+    val sol = monMap.toList.filter( t => !t._2.isEmpty )
+    
+    println(sol.head._2)
+    Grid.print(combinedOs(sol.head._1))
+    
+    val monCoords = sol.head._2.toSet
+    val hashCoords = charCount( combinedOs(sol.head._1), '#' )
+    println(hashCoords.size)
+    
+    println(monCoords.size)
+    println(hashCoords.size)
+    println(hashCoords.size - monCoords.size)
+    
+    
+  }
+  
+  def charCount( grid : Grid, ch : Char ) : List[(Int,Int)] = {
+    
+    val res = ListBuffer[(Int,Int)]()
+    
+    for( r <- 0 until grid.rows) {
+      for( c <- 0 until grid.cols ) {
+        if( grid(r,c) == ch ) {
+          val t = (r,c)
+          res += t
+        }
+      }
+    }
+    
+    res.toList
+    
+  }
+  
+  // assume that pattern and target are the same size
+  def matches( pattern : Grid, target : Grid ) : (Boolean,List[(Int,Int)]) = {
+    var m = true
+    val ts = ListBuffer[(Int,Int)]()
+    
+    for( r <- 0 until pattern.rows ) {
+      for( c <- 0 until pattern.cols ) {
+        if( pattern(r,c) == '#' ) { 
+          if( target(r,c) != '#' ) {
+            m = false
+          }
+          else {
+            val t = (r, c)
+            ts += t
+          }
+        }
+      }
+    }
+    
+    (m,ts.toList)
+  }
+  
+  def subGrid( src : Grid, rs : Int, cs : Int, height : Int, width : Int ) : Grid = {
+    val out = Grid( height, width )
+    
+    for( r <- 0 until height ) {
+      for( c <- 0 until width ) {
+        out(r,c, src( rs + r, cs + c ))
+      }
+    }
+    out
+  }
+  
+  def makeMonster() : Grid = {
+    val ls = monster.split("\n").toList.map( _.trim )
+    
+    val m = Grid( 3, 20 )
+    
+    for( r <- 0 until 3 ) {
+      val line = ls(r)
+      for( c <- 0 until 20 ) {
+        m( r, c, line(c) )
+      }
+    }
+    
+    
+    m
+  }
+  
+  val monster =
+    """..................#.
+       #....##....##....###
+       .#..#..#..#..#..#..."""
+
+  val mash1 =
+    """..#......#........#.
+       #....##.#..##...####
+       .#..#..#..#..#..#.#."""
+
+  val mash2 =
+    """..................#.
+       #.....#....##.....##
+       .#..#..#..#..#..#..."""
+       
+  def interior( src : Tile ) : Tile = {
+    
+    val ng = Grid( src.data.rows - 2, src.data.cols - 2 )
+    // println( s"${ src.data.rows - 2 } , ${ src.data.cols - 2 }" )
+    
+    for( r <- 1 until src.data.rows - 1 ) {
+      for( c <- 1 until src.data.cols - 1 ) {
+        ng( r - 1, c - 1, src.data( r, c ))
+      }
+    }
+    
+    Tile( src.id, ng )
+  }
+  
+  def matchSide( target : List[Char], allTiles : List[Tile] ) : Option[Tile] = {
+    allTiles.find( t => { edges(t).exists( lineup( target, _ ) ) })
+  }
+
+  def others( src : Tile, all : List[Tile] ) = { all.filter( t => { !t.id.equals(src.id) } ) }
+  
+  def cloneTile( src : Tile, g : Grid ) = { Tile( src.id, g ) }
+
+  def matchCounts( target : Tile, allTiles : List[Tile] ) : (Int,Int,Int,Int) = {
+
+    // get the top
+    val top = target.data.row(0)
+    val bottom = target.data.row(target.data.rows - 1)
+    val left = target.data.col(0)
+    val right = target.data.col(target.data.cols - 1)
+
+    //
+    val others = allTiles.filter( t => { !t.id.equals(target.id) } )
+
+    val allEdges = others.map( edges( _ ) ).flatten
+
+    val t = allEdges.count( e => lineup( e, top ))
+    val b = allEdges.count( e => lineup( e, bottom ))
+    val l = allEdges.count( e => lineup( e, left ))
+    val r = allEdges.count( e => lineup( e, right ))
+
+    (t,r,b,l)
+
   }
   
   def matchCount( target : Tile, allTiles : List[Tile] ) : Int = {
